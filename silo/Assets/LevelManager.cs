38b6edbc9624f37b8742bf0f9757ab8b517a83c6
Richard Kelsey <richard.kelsey12@gmail.com>
2023-09-14 12:45:51 +0100

    Beginning of level manager

--- silo/Assets/LevelManager.cs
+++ silo/Assets/LevelManager.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
 
 public class LevelManager : MonoBehaviour
 {
+    
+    public  Tilemap obstacleMap;
+    
+    private TileBase[] _obstacleTiles;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var position in obstacleMap.cellBounds.allPositionsWithin)
+        {
+            var tile = obstacleMap.GetTile(position);
+            Debug.Log(tile != null ? tile.name : "null tile");
+        }
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    
+    private void CreateSpawnpoints()
     {
         
     }
