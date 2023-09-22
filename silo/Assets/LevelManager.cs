ecdd23378903872a985b6ef9d4f8a248416011d4
Richard Kelsey <richard.kelsey12@gmail.com>
2023-09-22 09:12:15 +0100

    basic spawnpoints without spawning function

--- silo/Assets/LevelManager.cs
+++ silo/Assets/LevelManager.cs
@@ -1,24 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
+using Random = Unity.Mathematics.Random;
 
 public class LevelManager : MonoBehaviour
 {
-    
+    public TileBase spawnpointTile;
     public  Tilemap obstacleMap;
     
-    private TileBase[] _obstacleTiles;
+    private List<Vector3Int> _obstacleTiles;
+    
+    public int numOfSpawnpoints;
     
     // Start is called before the first frame update
     void Start()
     {
+        _obstacleTiles = new List<Vector3Int>();
+        int i = 0;
         foreach (var position in obstacleMap.cellBounds.allPositionsWithin)
         {
             var tile = obstacleMap.GetTile(position);
-            Debug.Log(tile != null ? tile.name : "null tile");
+            if (tile != null)
+            {
+                _obstacleTiles.Add(position);
+                i++;
+            } 
         }
+        
+        CreateSpawnpoints();
     }
 
     // Update is called once per frame
@@ -29,6 +41,9 @@ public class LevelManager : MonoBehaviour
     
     private void CreateSpawnpoints()
     {
-        
+        for (int i = 0; i < numOfSpawnpoints; i++)
+        {
+            obstacleMap.SetTile(_obstacleTiles[UnityEngine.Random.Range(0, _obstacleTiles.Count + 1)], spawnpointTile);
+        }
     }
 }
